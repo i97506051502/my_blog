458b6c35ba95549e72f5d1142c58db829c49ce8b
Go Sato <i97506051502@gmail.com>
2015-06-14 14:34:00 +0900

    ひととおり完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    @authors = Author.all
   end
 
   def show
@@ -12,11 +13,27 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     #@post = Post.new(params[:post])
     @post = Post.new(post_params)
     @post.save
     redirect_to "/posts/#{@post.id}"
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+    redirect_to "/posts/#{@post.id}"
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to "/posts"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
 
-  #resources :post
+  resources :posts
 
-  get '/posts' => 'posts#index'
-
-  get '/posts/new' => 'posts#new'
-
-  post '/posts' => 'posts#create'
-
-  get '/posts/:id' => 'posts#show'
+  # get '/posts' => 'posts#index'
+  # get '/posts/new' => 'posts#new'
+  # get '/posts/:id/edit' => 'posts#edit'
+  # post '/posts' => 'posts#create'
+  # get '/posts/:id' => 'posts#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'posts#index'
+  # root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
